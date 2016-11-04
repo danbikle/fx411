2437a54ff6cb71ef099fadc4f40904ba2c8e633e
Dan Bikle <bikle@bikle.com>
2016-11-03 20:47:53 -0700

    more

--- script/agg_zip.py
+++ script/agg_zip.py
@@ -5,20 +5,19 @@
 # Finally it should output a CSV file with aggregated current_price.
 
 import pandas as pd
-pairmo = 'USDJPY-2016-09'
-zipf   = '/home/fx411/truefx/'+pairmo+'.zip'
-f10_df = pd.read_csv('/home/fx411/truefx/USDJPY-2016-09.zip', names=['pair','ts0','bid','ask'])
-f11_df       =  f10_df.copy()[['pair','ts0']]
-f11_df['cp'] = (f10_df.bid+f10_df.ask)/2
-ts1_l        = [ts[:14] for ts in f11_df.ts0]
-ts2_sr       = pd.to_datetime(ts1_l, utc=True)
-f11_df['ts'] = [5*60*int(int(ts.strftime("%s"))/5/60) for ts in ts2_sr]
-f12_df       = f11_df.copy()[['ts','cp']]
-print(f12_df.tail())
-f13_df = f12_df.groupby(['ts']).cp.mean()
-print(f13_df.head())
-print(f13_df.tail())
-
-f13_df.to_csv('/home/fx411/csv/'+pairmo+'.csv', float_format='%4.6f')
+import glob
+fn_l = sorted(glob.glob("/home/fx411/truefx/USD*-2016-09.zip"))
+for pairmo_s in fn_l:
+  print(pairmo_s[19:33])
+  zipf   = '/home/fx411/truefx/'+pairmo_s+'.zip'
+  f10_df = pd.read_csv('/home/fx411/truefx/USDJPY-2016-09.zip', names=['pair','ts0','bid','ask'])
+  f11_df       =  f10_df.copy()[['pair','ts0']]
+  f11_df['cp'] = (f10_df.bid+f10_df.ask)/2
+  ts1_l        = [ts[:14] for ts in f11_df.ts0]
+  ts2_sr       = pd.to_datetime(ts1_l, utc=True)
+  f11_df['ts'] = [5*60*int(int(ts.strftime("%s"))/5/60) for ts in ts2_sr]
+  f12_df       = f11_df.copy()[['ts','cp']]
+  f13_df = f12_df.groupby(['ts']).cp.mean()
+  f13_df.to_csv('/home/fx411/csv/'+pairmo_s+'.csv', float_format='%4.6f')
 
 'bye'
