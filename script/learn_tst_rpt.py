5dec52557efa70751eb2fb0987daae9b01c4bdff
Dan Bikle <bikle@bikle.com>
2016-11-04 13:51:19 -0700

    more

---
+++ script/learn_tst_rpt.py
@@ -0,0 +1,18 @@
+# learn_tst_rpt.py
+
+# This script should learn from these files:
+# ../csv/featAUDUSD.csv
+# ../csv/featEURUSD.csv
+# ../csv/featGBPUSD.csv
+# ../csv/featUSDCAD.csv
+# ../csv/featUSDJPY.csv
+# Then it should generate predictions and write to these files:
+# ../csv/predictionsAUDUSD.csv
+# ../csv/predictionsEURUSD.csv
+# ../csv/predictionsGBPUSD.csv
+# ../csv/predictionsUSDCAD.csv
+# ../csv/predictionsUSDJPY.csv
+# Next it should report effectiveness and accuracy of the predictions.
+
+
+   
