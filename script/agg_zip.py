1cc035666ec77088b0d27520fe46d658d7ea0fbb
Dan Bikle <bikle@bikle.com>
2016-11-03 19:58:35 -0700

    more

--- script/agg_zip.py
+++ script/agg_zip.py
@@ -5,7 +5,8 @@
 # Finally it should output a CSV file with aggregated current_price.
 
 import pandas as pd
-
+pairmo = 'USDJPY-2016-09'
+zipf   = '/home/fx411/truefx/'+pairmo+'.zip'
 f10_df = pd.read_csv('/home/fx411/truefx/USDJPY-2016-09.zip', names=['pair','ts0','bid','ask'])
 f11_df       =  f10_df.copy()[['pair','ts0']]
 f11_df['cp'] = (f10_df.bid+f10_df.ask)/2
@@ -18,6 +19,6 @@ f13_df = f12_df.groupby(['ts']).cp.mean()
 print(f13_df.head())
 print(f13_df.tail())
 
-f13_df.to_csv('/home/fx411/csv/jpy.csv', float_format='%4.6f')
+f13_df.to_csv('/home/fx411/csv/'+pairmo+'.csv', float_format='%4.6f')
 
 'bye'
