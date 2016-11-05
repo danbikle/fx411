16bd9721f1d0ba2b9f331904090bee0802d55846
Dan Bikle <bikle@bikle.com>
2016-11-04 17:30:28 -0700

    more

--- script/learn_tst_rpt.py
+++ script/learn_tst_rpt.py
@@ -30,7 +30,7 @@ import pdb
 # jump size should be jump_i.
 # The number of jumps should be jumpc_i.
   
-wlen_i      = 100
+wlen_i      = 1000
 jump_i      = wlen_i # Avoids prediction 'overlap'
 trainsize_i = 4000 # Size of training data before the window AKA the test-window.
 # I should define the number of observations I hold a pair after I buy/sell it.
@@ -43,7 +43,7 @@ for pair_s in pairs_l:
   test_end_i = len(p0_df)
   # I should control how many times I jump the window.
   # If each jump is small, I can make more jumps:
-  jumpc_i    = int((len(p0_df)-trainsize_i-100) / jump_i)
+  jumpc_i    = int((len(p0_df)-trainsize_i-100) / jump_i)-1
   # Above expression keeps my jumps inside of p0_df.
 for cnt_i in range(jumpc_i,0,-1):
     # I should build a model here
@@ -51,6 +51,7 @@ for cnt_i in range(jumpc_i,0,-1):
     train_end_i   = test_start_i - 2*duration_i # Avoid overlap
     train_start_i = train_end_i - trainsize_i
     train_df      = p0_df[train_start_i:train_end_i]
+    train_df.to_csv('/tmp/train_df.csv', float_format='%4.4f')
     test_df       = p0_df[test_start_i:test_end_i]
     test_end_i   -= jump_i
     logr_model    = linear_model.LogisticRegression()
