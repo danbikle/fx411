44fce27219149f6965c3d507245eed436cda381f
Dan Bikle <bikle@bikle.com>
2016-11-05 00:35:58 -0700

    more

--- script/learn_tst_rpt.py
+++ script/learn_tst_rpt.py
@@ -7,17 +7,15 @@
 # ../csv/featUSDCAD.csv
 # ../csv/featUSDJPY.csv
 # Then it should generate predictions and write to these files:
-# ../csv/predictionsAUDUSD.csv
-# ../csv/predictionsEURUSD.csv
-# ../csv/predictionsGBPUSD.csv
-# ../csv/predictionsUSDCAD.csv
-# ../csv/predictionsUSDJPY.csv
-# Next it should report effectiveness and accuracy of the predictions.
+# ../csv/predictions_AUDUSD...
+# ../csv/predictions_EURUSD...
+# ../csv/predictions_GBPUSD...
+# ../csv/predictions_USDCAD...
+# ../csv/predictions_USDJPY...
 
 import pandas as pd
 import numpy  as np
 from sklearn import linear_model
-
 import pdb
 
 # I should use a nested loop to generate predictions from a window which slides over several pairs.
@@ -30,9 +28,9 @@ import pdb
 # jump size should be jump_i.
 # The number of jumps should be jumpc_i.
   
-wlen_i      = 100
+wlen_i      = 50
 jump_i      = wlen_i # Avoids prediction 'overlap'
-trainsize_i = 4000 # Size of training data before the window AKA the test-window.
+trainsize_i = 3000 # Size of training data before the window AKA the test-window.
 # I should define the number of observations I hold a pair after I buy/sell it.
 # Observations are separated by 5 min. One hour is 12 observations:
 duration_i = 12 # Hold for 1 hour then act on next prediction.
@@ -40,10 +38,13 @@ pairs_l    = ['AUDUSD','EURUSD','GBPUSD','USDCAD','USDJPY']
 for pair_s in pairs_l:
   print(pair_s)
   p0_df      = pd.read_csv("../csv/feat"+pair_s+".csv")
-  test_end_i = len(p0_df)
+  test_end_i = len(p0_df)-duration_i # avoid observations where piplead unknown.
   # I should control how many times I jump the window.
   # If each jump is small, I can make more jumps:
   jumpc_i    = int((len(p0_df)-trainsize_i-100) / jump_i)-1
+  # debug
+  jumpc_i = 200
+  # debug
   # Above expression keeps my jumps inside of p0_df.
   for cnt_i in range(jumpc_i,0,-1):
     # I should build a model here
@@ -56,14 +57,13 @@ for pair_s in pairs_l:
     test_end_i   -= jump_i
     logr_model    = linear_model.LogisticRegression()
     xtrain_a      = np.array(train_df)[:,3:]
-    xtest_a      = np.array(test_df)[:,3:]
+    xtest_a       = np.array(test_df)[:,3:]
     ytrain_sr     = train_df.piplead
     class_train_a = (ytrain_sr > 0.0)
     logr_model.fit(xtrain_a, class_train_a)
     # I should predict
-    predictions_l = logr_model.predict_proba(xtest_a)[:,1].tolist()
     predictions_df = test_df.copy()[['ts','cp','piplead']]
-    predictions_df['prediction'] = predictions_l
+    predictions_df['prediction'] = logr_model.predict_proba(xtest_a)[:,1].tolist()
     predictions_df['eff'] = np.sign(predictions_df.prediction - 0.5) * predictions_df.piplead
     predictions_df['acc'] = (predictions_df.eff > 0)
     fn_s = "../csv/predictions_"+pair_s+str(1000+cnt_i)+".csv" 
--- script/rpt.py
+++ script/rpt.py
@@ -9,19 +9,19 @@ import pdb
 
 pairs_l = ['AUDUSD','EURUSD','GBPUSD','USDCAD','USDJPY']
 for pair_s in pairs_l:
-    fn_l = glob.glob("../csv/predictions_"+pair_s+"*.csv")
-    # For this pair I should sort and make uniq and output to single file
-    # inspiration:
-    # sort -u ../csv/predictions_AUDUSD*.csv|grep 0> ../csv/predictionsAUDUSD.csv
-    if len(fn_l) > 0 :
-        cmd0_s = "sort -u ../csv/predictions_"+pair_s+"*.csv|grep 0 > "
-        fn_s   = "../csv/predictions"+pair_s+".csv"
-        cmd1_s = cmd0_s + fn_s
-        print(cmd1_s)
-        os.system(cmd1_s)
-        p0_df = pd.read_csv(fn_s,names=['ts','cp','piplead','eff','acc'])
-        print(p0_df.head())
-        print(pair_s+" Effectiveness:")
-        print(np.sum(p0_df.eff))
+  fn_l = glob.glob("../csv/predictions_"+pair_s+"*.csv")
+  # For this pair I should sort and make uniq and output to single file
+  # inspiration:
+  # sort -u ../csv/predictions_AUDUSD*.csv|grep 0> ../csv/predictionsAUDUSD.csv
+  if len(fn_l) > 0 :
+    cmd0_s = "sort -u ../csv/predictions_"+pair_s+"*.csv|grep 0 > "
+    fn_s   = "../csv/predictions"+pair_s+".csv"
+    os.system(cmd0_s + fn_s)
+    p0_df = pd.read_csv(fn_s,names=['ts','cp','piplead','prediction','eff','acc'])
+    print(p0_df.head())
+    print(pair_s+" Effectiveness:")
+    print(np.sum(p0_df.eff))
+    print(pair_s+" Accuracy:")
+    print(100 * np.sum(p0_df.acc) / len(p0_df.acc))
 
 'bye'
