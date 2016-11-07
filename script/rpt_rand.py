d175434a0566483171981504b958d37f5a3ee2f4
Dan Bikle <bikle@bikle.com>
2016-11-07 14:05:45 -0800

    more

--- script/rpt_rand.py
+++ script/rpt_rand.py
@@ -7,16 +7,34 @@
 # ts, price, piplead, eff, acc
 
 # I should create a report which has these columns:
-# trainsize, pair, eff_sum, acc_pct
+# trainsize, eff_sum, acc_pct
 
 import pandas as pd
+import numpy  as np
 import pdb
-import glob as glob
+import glob
 
 trainsize_i = 93000
 pair_s      = 'EURUSD'
-r0_df = pd.read_csv('../csv/predictions93000EURUSD.csv',names=['ts','price','piplead','eff','acc'])
+r0_df = pd.read_csv('../csv/predictions93000EURUSD.csv',names=['ts','price','piplead','prediction','eff','acc'])
 print(r0_df.head())
+
+# I should aggregate
+eff_sum = np.sum(r0_df.eff)
+print(eff_sum)
+acc_pct = np.round(100*np.sum(r0_df.acc) / len(r0_df),1)
+print(acc_pct)
+
+fn_l = glob.glob('../csv/predictions*.csv')
+print(fn_l[:9])
+
+all_sum = 0
+for fn in fn_l:
+    r0_df = pd.read_csv(fn,names=['ts','price','piplead','prediction','eff','acc'])
+    eff_sum = np.sum(r0_df.eff)
+    print(eff_sum)
+    all_sum = all_sum+eff_sum
+print(all_sum)
 
 'bye'
 
