9cf68af9e67b588f404e71ae61f66d30235f08cb
Dan Bikle <bikle@bikle.com>
2016-11-09 10:41:18 -0800

    more

---
+++ script/plotbg11.py
@@ -0,0 +1,23 @@
+# plotbg11.py
+
+# This script should transform a csv file into a DataFrame and then into blue-green-visualization.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import pdb
+
+# I should get the filename from the command line.
+
+# I should check cmd line args
+import sys
+if (len(sys.argv) != 2):
+  print('You typed something wrong:')
+  print('Demo:')
+  print("~/anaconda3/bin/python plotbg11.py ../csv/predictions31000AUDUSD.csv")
+  sys.exit()
+
+csv_in = sys.argv[1]
+p0_df  = pd.read_csv(csv_in)
+print(p0_df.head())
+
+'bye'
