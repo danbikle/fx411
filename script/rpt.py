48a38350ccc43427407832959f9a2cca59599569
Dan_Bikle <bikle101@gmail.com>
2016-11-08 21:43:07 -0800

    more

--- script/rpt.py
+++ script/rpt.py
@@ -1,27 +1,40 @@
 # rpt.py
 
-# This script should report accuracy and effectiveness of predictions in ../csv/predictions_*.csv
+# This script should help me find most predictable pair.
+# This script should help me find optimal training sizes.
 
-import os,glob
+
+# The predictions are in files which look like this:
+#   -rw-rw-r--  1 fx411 fx411   274782 Nov  7 08:28 predictions93000EURUSD.csv
+
+# The columns are listed below:
+# ts, price, piplead, eff, acc
+
 import pandas as pd
 import numpy  as np
 import pdb
+import glob
 
-pairs_l = ['AUDUSD','EURUSD','GBPUSD','USDCAD','USDJPY']
-for pair_s in pairs_l:
-  fn_l = glob.glob("../csv/predictions_"+pair_s+"*.csv")
-  # For this pair I should sort and make uniq and output to single file
-  # inspiration:
-  # sort -u ../csv/predictions_AUDUSD*.csv|grep 0> ../csv/predictionsAUDUSD.csv
-  if len(fn_l) > 0 :
-    cmd0_s = "sort -u ../csv/predictions_"+pair_s+"*.csv|grep 0 > "
-    fn_s   = "../csv/predictions"+pair_s+".csv"
-    os.system(cmd0_s + fn_s)
-    p0_df = pd.read_csv(fn_s,names=['ts','cp','piplead','prediction','eff','acc'])
-    print(p0_df.head())
-    print(pair_s+" Effectiveness:")
-    print(np.sum(p0_df.eff))
-    print(pair_s+" Accuracy:")
-    print(str(100 * np.sum(p0_df.acc) / len(p0_df.acc))+' %')
+fn_l = glob.glob('../csv/predictions*.csv')
+
+all_sum = 0
+pair_trainsize_eff_acc_l = []
+for fn in sorted(fn_l):
+    r0_df = pd.read_csv(fn,names=['ts','price','piplead','prediction','eff','acc'])
+    eff_sum     = np.sum(r0_df.eff)
+    all_sum     = all_sum+eff_sum
+    acc_pct     = np.round(100*np.sum(r0_df.acc) / len(r0_df),1)
+    trainsize_i = fn[18:-10]
+    pair_s      = fn[-10:-4]
+    pair_trainsize_eff_acc_l.append([pair_s,trainsize_i,eff_sum,acc_pct])
+# I should convert pair_trainsize_eff_acc_l into a DataFrame.
+pair_trainsize_eff_acc_a  = np.array(pair_trainsize_eff_acc_l)
+pair_trainsize_eff_acc_df = pd.DataFrame({'pair':  pair_trainsize_eff_acc_a[:,0]
+                                      ,'trainsize':pair_trainsize_eff_acc_a[:,1]
+                                      ,'eff':      pair_trainsize_eff_acc_a[:,2]
+                                      ,'acc':      pair_trainsize_eff_acc_a[:,3]
+})
+print(pair_trainsize_eff_acc_df.head())
+
 
 'bye'
