b16a9793d82f6d5d9e5b6dffe3f3ddbdc13ec36a
Dan Bikle <bikle@bikle.com>
2017-02-06 19:48:50 -0800

    more

--- script/truefx_get.bash
+++ script/truefx_get.bash
@@ -3,6 +3,7 @@
 # truefx_get.bash
 
 cd ~/fx411/
+export PATH=${HOME}/fx411/bin:$PATH
 # The script below should fail because it has wrong user/pass:
 bin/rspec spec/features/truefx_spec.rb
 # You should get account at truefx.com
--- spec/features/truefx_spec.rb
+++ spec/features/truefx_spec.rb
@@ -18,25 +18,27 @@ describe 'This should start Chrome', :js => true do
     find(:xpath, "//a[@href='?page=logina']").click
     sleep 1
     within("#login-form") do
-      fill_in 'USERNAME', with: 'bob1999'
+      fill_in 'USERNAME', with: 'bobsmithIV'
       sleep 1
-      fill_in 'PASSWORD', with: 'year2k3k'
+      fill_in 'PASSWORD', with: 'eurosDOLLASy3n'
       sleep 1
       click_on 'Login'
       sleep 1
     end
+    years_a    = [2010,2011,2012,2013,2014,2015,2016,2017]
     months_s_a = ['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER']
-    months_i_a = ['01','02','03','04','05','06','07','08','09','10','11','12']
-    m_i        = -1 # I should use this to count months in the loop below.
-    years_a    = [2010,2011,2012,2013,2014,2015,2016]
     pairs_a    = ['AUDUSD','EURUSD','GBPUSD','USDCAD','USDJPY']
-    for month_s in months_s_a do
-      m_i += 1
-      for yr_i in years_a do
-        for pair in pairs_a do
-          p "#{ahost}/dev/data/#{yr_i}/#{month_s}-#{yr_i}/#{pair}-#{yr_i}-#{months_i_a[m_i]}.zip"
-          visit     "/dev/data/#{yr_i}/#{month_s}-#{yr_i}/#{pair}-#{yr_i}-#{months_i_a[m_i]}.zip"
-          sleep 40
+    for yr_i       in years_a    do
+      for month_s  in months_s_a do
+        for pair_s in pairs_a    do
+          click_link('Downloads')
+          sleep 2
+          click_link(String(yr_i))
+          sleep 2
+          click_link(month_s.capitalize)
+          sleep 2
+          click_link(pair_s)
+          sleep 33
         end
       end
     end
